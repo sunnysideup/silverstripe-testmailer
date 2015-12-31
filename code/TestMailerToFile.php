9c2b0a429352ce9ca2c8a5dc95ecdcac08000c46
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:33:43 +1300

    Converted to PSR-2

--- code/TestMailerToFile.php
+++ code/TestMailerToFile.php
@@ -5,45 +5,47 @@
  * @subpackage email
  */
 
-class TestMailerToFile extends Mailer  {
+class TestMailerToFile extends Mailer
+{
 
-	private static $show_all_details = false;
+    private static $show_all_details = false;
 
-	private static $file_to_write_to = "assets/emails.txt";
+    private static $file_to_write_to = "assets/emails.txt";
 
-	private static $separation_string = "
+    private static $separation_string = "
 -------------------------------------------------------------------------------------------------------------------------------------";
 
-	/**
-	 * Send a plain-text email
-	 */
-	function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = false, $customheaders = false) {
-		if($this->Config()->get("show_all_details")) {
-			$string = "
+    /**
+     * Send a plain-text email
+     */
+    public function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = false, $customheaders = false)
+    {
+        if ($this->Config()->get("show_all_details")) {
+            $string = "
 ~TO: $to ~
 ~FROM: $from ~
 ~SUBJECT: $subject ~
 ~PLAINTEXT: $plainContent ~
 ~ATTACHEDFILES: ".print_r($attachedFiles, 1)." ~
 ~CUSTOMHEADERS: ".print_r($customheaders, 1)." ~";
-		}
-		else {
-			$string = "
+        } else {
+            $string = "
 ~PLAINTEXT: TRUE
 ~TO: $to ~
 ~FROM: $from ~
 ~SUBJECT: $subject ~";
-		}
-		$this->writeToFile($string);
-		return true;
-	}
+        }
+        $this->writeToFile($string);
+        return true;
+    }
 
-	/**
-	 * Send a multi-part HTML email
-	 */
-	function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false, $inlineImages = false) {
-		if($this->Config()->get("show_all_details")) {
-			$string = "
+    /**
+     * Send a multi-part HTML email
+     */
+    public function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false, $inlineImages = false)
+    {
+        if ($this->Config()->get("show_all_details")) {
+            $string = "
 ~TO: $to ~
 ~FROM: $from ~
 ~SUBJECT: $subject ~
@@ -52,24 +54,23 @@ class TestMailerToFile extends Mailer  {
 ~CUSTOMHEADERS: ".print_r($customheaders, 1)." ~
 ~PLAINTEXT: $plainContent ~
 ~INLINEIMAGES: $inlineImages ~";
-		}
-		else {
-			$string = "
+        } else {
+            $string = "
 ~PLAINTEXT: FALSE
 ~TO: $to ~
 ~FROM: $from ~
 ~SUBJECT: $subject ~";
-		}
-		$this->writeToFile($string);
-		return true;
-	}
+        }
+        $this->writeToFile($string);
+        return true;
+    }
 
-	protected function writeToFile($string) {
-		$myFile = Director::baseFolder()."/".self::$file_to_write_to;
-		$fh = fopen($myFile, 'a') or die("can't open file $myFile");
-		$stringData = self::$separation_string.$string;
-		fwrite($fh, $stringData);
-		fclose($fh);
-	}
-
+    protected function writeToFile($string)
+    {
+        $myFile = Director::baseFolder()."/".self::$file_to_write_to;
+        $fh = fopen($myFile, 'a') or die("can't open file $myFile");
+        $stringData = self::$separation_string.$string;
+        fwrite($fh, $stringData);
+        fclose($fh);
+    }
 }
